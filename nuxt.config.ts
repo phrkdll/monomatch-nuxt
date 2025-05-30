5cc30cc72950930caad2721c8256b26e4faa05f4
phrkdll <benjamin.hotop@gmail.com>
2025-05-30 16:08:14 +0200

    add runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
-  css: ["@picocss/pico/css/pico.amber.min.css"], 
-  eslint: {
-    config: {
-      stylistic: {
-        semi: false,
-        quotes: "double",
-        commaDangle: "always-multiline",
-        indent: "tab",
-      },
-    },
-  },
+	modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
+	devtools: { enabled: true },
+	css: ["@picocss/pico/css/pico.amber.min.css"],
+	runtimeConfig: {
+		public: {
+			API_BASE_URL: process.env.API_BASE_URL,
+		},
+	},
+	compatibilityDate: "2025-05-15",
+	eslint: {
+		config: {
+			stylistic: {
+				semi: false,
+				quotes: "double",
+				commaDangle: "always-multiline",
+				indent: "tab",
+			},
+		},
+	},
 })
